cb7b07c8a3279a765476a481e6765bfdcf4b62ca
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2023-01-04 19:13:24 +0100

    Enumerate some processes

--- nightview.c
+++ nightview.c
@@ -1,7 +1,32 @@
+#include <windows.h>
+#include <tlhelp32.h>
+#include <tchar.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 int main ( ) {
-    printf("Hello, World!\n");
+    return enumerate_processes();
+}
+
+int enumerate_processes ( ) {
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if ( snapshot == INVALID_HANDLE_VALUE ) {
+        printf("Failed to create snapshot\n");
+        return 1;
+    }
+
+    PROCESSENTRY32 pe;
+    pe.dwSize = sizeof(pe);
+
+    if ( !Process32First(snapshot, &pe) ) {
+        printf("Failed to enumerate first process\n");
+        return 1;
+    }
+
+    do {
+        printf("%6d | %s\n", pe.th32ProcessID, pe.szExeFile);
+    } while( Process32Next(snapshot, &pe) );
+
+    CloseHandle(snapshot);
+
     return 0;
 }
